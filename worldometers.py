1fd0179b3bfef55de29e40efbac2e637b60ad316
youssouf1994 <youssouf.emin94@gmail.com>
2020-03-14 15:49:33 -0400

    collect data for today

--- worldometers.py
+++ worldometers.py
@@ -7,24 +7,18 @@ Created on Sat Mar 14 14:06:15 2020
 """
 
 import csv
-import sys
 from datetime import date
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from os import path, mkdir
 
-data_path = "data"
-
-if len(sys.argv) >= 2:
-    data_path = sys.argv[1]
-
-if not path.exists(data_path) or not path.isdir(data_path):
-    mkdir(data_path)
-
 url = "https://www.worldometers.info/coronavirus/"
-
 driver = webdriver.Chrome()
 driver.get(url)
+
+data_path = "data"
+if not path.exists(data_path) or not path.isdir(data_path):
+    mkdir(data_path)
 
 table_id = 'main_table_countries'
 header_tag = "thead"
@@ -46,15 +40,13 @@ rows = table.find_elements(By.TAG_NAME, row_tag) # get all of the rows in the ta
 
 today = date.today()
 
-try:
-    with open(data_path + '/' + str(today) + '.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([header.text.replace('</br>', '') for header in headers])
-        for row in rows:
-            cols = row.find_elements(By.TAG_NAME, col_tag) # get all the columns of the row
-            writer.writerow([col.text for col in cols])
-except:
-    print("Error has occuring while writing the file")
+with open(data_path + '/' + str(today) + '.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow([header.text.replace('</br>', '') for header in headers])
+    for row in rows:
+        cols = row.find_elements(By.TAG_NAME, col_tag) # get all the columns of the row
+        writer.writerow([col.text for col in cols])
+
 
 driver.close()
 driver.quit()
