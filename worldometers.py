ef6509980922ad951a9d63c0d49308555cfd1ff9
youssouf1994 <youssouf.emin94@gmail.com>
2020-03-14 21:04:00 -0400

    use beautifulsoup instead of selenium

--- worldometers.py
+++ worldometers.py
@@ -6,47 +6,44 @@ Created on Sat Mar 14 14:06:15 2020
 @author: youssouf
 """
 
+import requests
 import csv
+from bs4 import BeautifulSoup
 from datetime import datetime
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import pandas as pd
 from os import path, mkdir
-
-url = "https://www.worldometers.info/coronavirus/"
-driver = webdriver.Chrome()
-driver.get(url)
 
 data_path = "data"
 if not path.exists(data_path) or not path.isdir(data_path):
     mkdir(data_path)
 
+url = "https://www.worldometers.info/coronavirus/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, features="html.parser")
+
 table_id = 'main_table_countries'
-header_tag = "thead"
-head_tag = "th"
-row_tag = "tr"
-col_tag = "td"
+header_tag = 'thead'
+head_tag = 'th'
+row_tag = 'tr'
+col_tag = 'td'
 
 # get the table
 
-table = driver.find_element(By.ID, table_id)
+table = soup.find(id=table_id)
 
 # get header of the table
 
-headers = table.find_elements(By.TAG_NAME, header_tag)
-headers = headers[0].find_elements(By.TAG_NAME, row_tag)
-headers = headers[0].find_elements(By.TAG_NAME, head_tag)
+headers = table.find_all(header_tag)[0]
+headers = headers.find_all(row_tag)[0]
+headers = headers.find_all(head_tag)
 
-rows = table.find_elements(By.TAG_NAME, row_tag) # get all of the rows in the table
+rows = table.find_all(row_tag) # get all of the rows in the table
 
 now = datetime.now()
 
-with open(data_path + '/' + now.strftime("%d-%m-%Y-%H:%M") + '.csv', 'w', newline='') as f:
+with open(data_path + '/' + now.strftime("%d-%m-%Y:%H:%M") + '.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow([header.text.replace('</br>', '') for header in headers])
+    writer.writerow([header.get_text().replace('</br>', ' ') for header in headers])
     for row in rows:
-        cols = row.find_elements(By.TAG_NAME, col_tag) # get all the columns of the row
+        cols = row.find_all(col_tag) # get all the columns of the row
         writer.writerow([col.text for col in cols])
-
-
-driver.close()
-driver.quit()
