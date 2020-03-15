15109ffa5fcbba6ea86a525a9f65643f3b873682
youssouf1994 <youssouf.emin94@gmail.com>
2020-03-15 12:25:46 -0400

    aggregate data

--- data_prepro.py
+++ data_prepro.py
@@ -3,22 +3,28 @@
 """
 Created on Sat Mar 15 14:06:15 2020
 
-@author: stef
+@author: stef, youssouf
 """
 
-import numpy as np
 import pandas as pd
-import os
+from os import path
 
-def agg_data(data_path, data_file):
-    df = pd.read_csv(os.path.join(data_path, data_file), sep=",") 
+FIELDS = {'country': 'Country,Other',
+    'total_cases': 'TotalCases',
+    'total_deaths' : 'TotalDeaths',
+    'total_recovered' : 'TotalRecovered'}
+
+data_path = path.join(path.dirname(path.abspath(__file__)),"data")
+
+def agg_data_bycountry(data_path, data_file):
+    df = pd.read_csv(path.join(data_path, data_file), sep=",") 
     # Aggregate data per country
     df = df.groupby('Country/Region')
     agg_df = df.sum().reset_index()
     return agg_df
 
 def add_local_data(data_path, full_data, new_data):    
-    df = pd.read_csv(os.path.join(data_path, new_data), sep=",") 
+    df = pd.read_csv(path.join(data_path, new_data), sep=",") 
     
 def get_updates_country(country, df, key = 'total_cases'):
     # returns updates deaths, new cases and recovered from new dataframe per country
@@ -35,11 +41,8 @@ def get_updates_country(country, df, key = 'total_cases'):
 
 
 def update_full_data(data_path, full_data, new_data, key = 'total_cases'):
-    df_0 = pd.read_csv(os.path.join(data_path, full_data), sep=",")
-    df = pd.read_csv(os.path.join(data_path, new_data), sep=",")
-    # add column new day
-    day = date.today().strftime('%-m/%d/%y')
-    df_0[day] = [0]*len(df_0)  
+    df_0 = pd.read_csv(path.join(data_path, full_data), sep=",")
+    df = pd.read_csv(path.join(data_path, new_data), sep=",")
     
     for country in df_0['Country/Region']:
         df_0.iloc[:,-1][df_0['Country/Region']==country] = get_updates_country(country, df, key)      
@@ -51,18 +54,12 @@ def update_full_data(data_path, full_data, new_data, key = 'total_cases'):
     df_0.to_csv(os.path.join(data_path, file_name))
     
 if __name__ == "__main__":
-	FIELDS = {'total_cases': 'Total\nCases',
-		 'total_deaths' : 'Total\nDeaths',
-		 'total_recovered' : 'Total\nRecovered'}
-    
-	# path to the three csv files 
-	data_path = "/Users/ebnou/Desktop/Projects/mauriSante/data"
-	data_files = ["time_series_19-covid-Confirmed-updated.csv", "time_series_19-covid-Deaths-updated.csv",
-		      "time_series_19-covid-Recovered-updated.csv"]
-    
-	# daily file from worldometer
-	new_data = '2020-03-15.csv'
+ 
+    data_files = ["CC_13-03-2020:00:00.csv", "DC_13-03-2020:00:00.csv",
+		      "DC_13-03-2020:00:00.csv"]
 
-	#update and save 
-    
-	[update_full_data(data_path, full_data, new_data, key) for (key,full_data) in zip(FIELDS.keys(), data_files)]
+    # daily file from worldometer
+    new_data = '2020-03-15.csv'
+
+    #update and save
+    [update_full_data(data_path, full_data, new_data, key) for (key,full_data) in zip(FIELDS.keys(), data_files)]
